2a8dd0b05086a5124163d835e6eb639309a637a3
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-12-29 15:13:41 +0900

    Refactor : 회원가입 버튼 생성 (Donny : 12월 29일 15:14분)

--- src/components/Login/LoginForm.test.tsx
+++
@@ -1,7 +0,0 @@
-// import { render, screen } from "@testing-library/react";
-// import "@testing-library/jest-dom";
-// import { LoginForm } from "../index";
-
-// test("인풋 창 테스트 입니다", () => {
-//   render(LoginForm);
-// });
--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -3,12 +3,18 @@ import styles from "./LoginForm.module.css";
 import eyeIcon from "../../assets/eyeIcon.svg";
 import closeEyeIcon from "../../assets/closeEyeIcon.svg";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export function LoginForm() {
   const [click, setClick] = useState(false);
+  const navigate = useNavigate();
 
   const handleMouseClick = () => {
     setClick((prev) => !prev);
+  };
+
+  const moveSignUpPage = () => {
+    navigate("/signup");
   };
 
   return (
@@ -48,6 +54,9 @@ export function LoginForm() {
           </>
         ))}
         <button className={styles.formButton}>로그인 해주세요</button>
+        <button className={styles.formButton} onClick={moveSignUpPage}>
+          회원가입
+        </button>
       </form>
     </>
   );
