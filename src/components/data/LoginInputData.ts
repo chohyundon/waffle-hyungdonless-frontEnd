cd92293fc20cbf9c2ce920d5826a5b8a725f9061
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-12-29 14:36:19 +0900

    Feat : 로그인 input 데이터 로직 분리 (Donny : 12월 29일 14:37분)

---
+++ src/components/data/LoginInputData.ts
@@ -0,0 +1,19 @@
+export const LoginInputData = [
+  {
+    id: "id",
+    placeholder: "아이디를 입력해주세요",
+    name: "id",
+    ariaLabel: "아이디",
+    maxLength: 12,
+    formGuide: "6~12자 영문, 숫자로 입력 해주세요",
+  },
+  {
+    id: "password",
+    placeholder: "비밀번호를 입력해주세요",
+    name: "password",
+    ariaLabel: "패스워드",
+    maxLength: 15,
+    formGuide:
+      "영문, 숫자를 혼합하여 6~12자로 입력 해주세요. (특수문자는 .!@#$% 만 허용됩니다.)",
+  },
+];
---
+++ src/components/data/index.ts
@@ -0,0 +1,1 @@
+export { LoginInputData } from "./LoginInputData";
