0cfbc4fb8b4f0e68b84098b7e2242ac40b2a5ee1
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-07 18:39:19 +0900

    Refactor : main.tsx에서 createRoot 타입오류 뜨는 것을 해결(Donny: 1월 7일 18시39분)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./components/routes/index.tsx";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
