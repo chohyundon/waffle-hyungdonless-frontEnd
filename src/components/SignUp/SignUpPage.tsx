496db6044764a699926a12820ed9d5533e0c0d65
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-08 22:28:18 +0900

    Feat : 회원가입 UI 마크업 (Donny: 1월 8일 22시 35분)

--- src/components/SignUp/SignUpPage.tsx
+++ src/components/SignUp/SignUpPage.tsx
@@ -1,7 +1,16 @@
+import styles from "./SginUp.module.css";
+import { SignUpForm } from "../index";
+import checkProcedure from "../../assets/signUpProcedure.svg";
+// import noneCheckProcedure from "../../assets/nonCheckCircle.svg";
+
 export function SignUpPage() {
   return (
-    <>
-      <p>SignUp</p>
-    </>
+    <div className={styles.signUpContainer}>
+      <main className={styles.mainContainer}>
+        <SignUpForm />
+        <img src={checkProcedure} />
+      </main>
+      <figure></figure>
+    </div>
   );
 }
