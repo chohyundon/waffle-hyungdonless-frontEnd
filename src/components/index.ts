f2bc5d13f0e84d9c38fbf1f060d47ac627686e77
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-08 22:27:36 +0900

    Feat : 회원가입 폼 컴포넌트 라우팅 (Donny: 1월 8일 22시 27분)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 export { LoginHomePage } from "./Login/LoginHome";
+
 export { LoginForm } from "./Login/LoginForm";
 
 export { SignUpPage } from "./SignUp/SignUpPage";
+export { SignUpForm } from "./SignUp/SignUpForm";
 
 export { GoogleLogin } from "./Login/GoogleLogin";
 
