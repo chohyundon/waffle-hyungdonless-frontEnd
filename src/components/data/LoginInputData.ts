e584b74dfaee9f0397a1663e12a6fab5f5b82d01
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-14 21:32:01 +0900

    Feat: 로그인, 회원가입에 필요한 타입 정리(Donny: 1월 14일 21시 32분)

--- src/components/data/LoginInputData.ts
+++ src/components/data/LoginInputData.ts
@@ -1,23 +1,4 @@
-import { LoginInputType } from "../types/LoginInputType";
-
-export const LoginInputData: LoginInputType[] = [
-  {
-    id: "id",
-    placeholder: "이메일",
-    name: "id",
-    ariaLabel: "아이디",
-    maxLength: 12,
-    formGuide: "6~12자 영문, 숫자로 입력 해주세요",
-    type: "text",
-  },
-  {
-    id: "password",
-    placeholder: "비밀번호",
-    name: "password",
-    ariaLabel: "패스워드",
-    maxLength: 15,
-    formGuide:
-      "영문, 숫자를 혼합하여 6~12자로 입력 해주세요. (특수문자는 .!@#$% 만 허용됩니다.)",
-    type: "password",
-  },
-];
+export interface LoginInputData {
+  email: string;
+  password: string;
+}
--- src/components/data/SignUpData.ts
+++ src/components/data/SignUpData.ts
@@ -1,27 +1,49 @@
-import { SignUpTypes } from "../types/SignUpTypes";
+type ButtonType ={
+  buttonText?: '다음' | '로그인' | '가입 완료' | '인증요청'
+}
 
-export const SignUpData: SignUpTypes[] = [
-  {
-    id: "email",
-    placeholder: "example@gmail.com",
-    name: "email",
-    type: "text",
-    data: "이메일",
-    formGuide: "이메일 형식에 맞게 입력해주세요.",
-  },
-  {
-    id: "password",
-    placeholder: "비밀번호",
-    name: "password",
-    type: "password",
-    data: "비밀번호",
-    defaultGuide: "영문 대·소문자/숫자/특수문자 중 2가지 이상 조합, 8자~32자",
-  },
-  {
-    id: "passwordCheck",
-    placeholder: "비밀번호 확인",
-    name: "passwordCheck",
-    type: "password",
-    formGuide: "비밀번호가 일치하지 않습니다. ",
-  },
-];
+export interface UseInputData {
+  type: 'text' | "password",
+  name: "email" | 'password' | 'passwordCheck',
+  label?: '이메일' | '비밀번호' | '이름' | '생년월일' | '휴대전화 인증' | '닉네임',
+  errorMessage: '이메일 형식에 맞게 입력해주세요.' | '비밀번호가 일치하지 않습니다.' | '비밀번호는 8자리 이상 작성해주세요.' | '존재하는 이메일입니다.'
+    | '인증번호가 일치하지 않습니다.' | '존재하는 닉네임입니다.',
+  placeholder: 'example@email.com' | '비밀번호' | '비밀번호 확인' | '1999  /  06  /  01' | '‘-’없이 숫자만 입력' | '통신사' | '인증번호 입력' | '홍준표'
+  id: "email" | 'password' | 'passwordCheck',
+  required?: '이메일은 필수입니다' | '비밀번호는 필수입니다.'
+}
+
+export const SignUpInputData: UseInputData[] = [{
+  type: 'text',
+  name: 'email',
+  label: '이메일',
+  placeholder: 'example@email.com',
+  errorMessage: '이메일 형식에 맞게 입력해주세요.',
+  id: 'email',
+  required: "이메일은 필수입니다",
+}, {
+  type: 'password',
+  name: 'password',
+  label: '비밀번호',
+  placeholder: '비밀번호',
+  errorMessage: '비밀번호는 8자리 이상 작성해주세요.',
+  id: 'password',
+  required: "비밀번호는 필수입니다.",
+}, {
+  type: 'password',
+  name: 'passwordCheck',
+  errorMessage: '비밀번호가 일치하지 않습니다.',
+  placeholder: "비밀번호 확인",
+  id: 'passwordCheck',
+  required: "비밀번호는 필수입니다.",
+}]
+
+export const ButtonText:ButtonType = {
+  buttonText: '다음',
+}
+
+export interface SignUpData  {
+  email: string
+  password: string
+  passwordCheck: string
+}
