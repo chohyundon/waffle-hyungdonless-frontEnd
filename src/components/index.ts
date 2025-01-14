4bd18ef74350479e5a252a645d16ffb5b500c565
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-14 21:29:52 +0900

    Feat : 회원가입 페이지 추가 (Donny: 1월 14일 21시 30분)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,16 @@ export { LoginHomePage } from "./Login/LoginHome";
 export { LoginForm } from "./Login/LoginForm";
 
 export { SignUpPage } from "./SignUp/SignUpPage";
-export { SignUpForm } from "./SignUp/SignUpForm";
+export { SignUpForm } from './SignUp/SignUpForm.tsx';
+
+export { SignUpFormFirstStep } from "./SignUp/SignUpFormFirstStep.tsx";
+export { SignUpFormSecondStep } from "./SignUp/SignUpFormSecondStep.tsx";
+export { SignUpFormThirdStep } from "./SignUp/SignUpFormThirdStep.tsx";
 
 export { GoogleLogin } from "./Login/GoogleLogin";
 
 export { SearchId } from "./Login/SearchId";
+
+export {SignUpStep} from "./SignUp/SignUpStep";
+
+export {LoginStatus} from "./Login/LoginStatus";
