e9dba3664cfd0a90550061f16c78f80327739103
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-22 01:13:23 +0900

    Feat : 회원가입 절차 로직 분리(Donny: 1월 22일 01시 13분)

--- src/components/SignUp/SignUpPage.tsx
+++ src/components/SignUp/SignUpPage.tsx
@@ -1,12 +1,10 @@
 import styles from "./SginUp.module.css";
-import { SignUpStep } from "../index";
 import {Outlet} from "react-router";
 
 export function SignUpPage() {
   return (
     <div className={styles.signUpContainer}>
         <Outlet/>
-        <SignUpStep/>
       <figure></figure>
     </div>
   );
