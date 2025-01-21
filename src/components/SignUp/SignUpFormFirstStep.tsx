c888cdc8118222fe2152074d2b0411d0df455bfd
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-22 01:06:02 +0900

    Feat : SignUpFormFirstStep 높이 지정을 따로 하기위해 로직 분리(Donny: 1월 22일 01시 06분)

--- src/components/SignUp/SignUpFormFirstStep.tsx
+++ src/components/SignUp/SignUpFormFirstStep.tsx
@@ -4,14 +4,15 @@ import {GoogleLogin} from "../Login/GoogleLogin.tsx";
 
 export function SignUpFormFirstStep() {
   return (
-    <>
-      <SignUpForm />
+    <main className={styles.mainContainer}>
+      <h1 className={styles.title}>회원가입</h1>
+      <SignUpForm/>
       <div className={styles.orContainer}>
-        <span className={styles.orBorder} />
+        <span className={styles.orBorder}/>
         <span className={styles.orFont}>또는</span>
-        <span className={styles.orBorder} />
+        <span className={styles.orBorder}/>
       </div>
-      <GoogleLogin />
-    </>
+      <GoogleLogin/>
+    </main>
   )
 }
--- src/components/SignUp/SignUpPage.tsx
+++ src/components/SignUp/SignUpPage.tsx
@@ -5,11 +5,8 @@ import {Outlet} from "react-router";
 export function SignUpPage() {
   return (
     <div className={styles.signUpContainer}>
-      <main className={styles.mainContainer}>
-        <h1 className={styles.title}>회원가입</h1>
         <Outlet/>
         <SignUpStep/>
-      </main>
       <figure></figure>
     </div>
   );
