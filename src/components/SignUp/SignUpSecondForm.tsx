80d5f60df30eafdbaa434dcee06d0ec403748204
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-01-24 15:03:04 +0900

    Feat : 회원가입 3번째 페이지 마크업 및 다지인(Donny: 1월 24일 15시 03분)

--- src/components/SignUp/SignUpSecondForm.tsx
+++ src/components/SignUp/SignUpSecondForm.tsx
@@ -1,40 +1,59 @@
 import styles from './SignUpSecondForm.module.css'
 import {useForm} from "react-hook-form";
 import {SignUpStepData} from "../data/SignUpData.ts";
+import {useNavigate} from "react-router";
+import {Delay} from "../util/Delay.ts";
 
 export const SignUpSecondForm = () => {
+  const navigate = useNavigate();
+
   const {register, handleSubmit, formState: {errors, isSubmitted}} = useForm<SignUpStepData>();
 
-  const sendSignUpStep2Value = (data:SignUpStepData) => {
-    console.log(data);
+  const sendSignUpStep2Value = async (data:SignUpStepData) => {
+    await fetch('https://api.sabujak.life/users/signup', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+
+    if(data) {
+      await Delay(2000);
+      navigate('/signUp/step3')
+    }
   }
 
  return (
-    <form className={styles.formContainer} onSubmit={handleSubmit(sendSignUpStep2Value)}>
-      <label className={styles.labelText}>이름</label>
-      <input type="text" className={styles.inputBox} placeholder="조현돈" aria-invalid={isSubmitted ? (errors.name ? 'true' : 'false' )  : undefined}
-             {...register("name", {
-        required: "이름 값 입력은 필수 입력입니다.",
-        pattern: {
-          value: /^[ㄱ-ㅎ|가-힣]+$/,
-          message: '한글로만 입력해주세요.'
-        },
-        maxLength: {
-          value: 5,
-          message: '이름은 5글자 미만으로 작성해주세요.'
-        }
-      })}/>
-      {errors.name && (<span className={styles.errorFont}>{errors.name.message}</span>)}
-      <div className={styles.inputBoxContainer}>
-        <label className={styles.labelText}>생년월일</label>
-        <input type="date" className={styles.inputBox}
-               aria-invalid={isSubmitted ? (errors.date ? 'true' : 'false') : undefined}
-               {...register("date", {
-                 required: "생년월일은 필수 입력입니다."
-               })}/>
-      </div>
-      {errors.date && (<span className={styles.errorFont1}>{errors.date.message}</span>)}
-      <button className={styles.subButton}>가입 완료</button>
-    </form>
+   <form className={styles.formContainer} onSubmit={handleSubmit(sendSignUpStep2Value)}>
+     <label className={styles.labelText}>이름</label>
+     <input type="text" className={styles.inputBox} placeholder="조현돈"
+            aria-invalid={isSubmitted ? (errors.name ? 'true' : 'false') : undefined}
+            {...register("name", {
+              required: "이름 값 입력은 필수 입력입니다.",
+              pattern: {
+                value: /^[ㄱ-ㅎ|가-힣]+$/,
+                message: '한글로만 입력해주세요.'
+              },
+              maxLength: {
+                value: 5,
+                message: '이름은 5글자 미만으로 작성해주세요.'
+              }
+            })}/>
+     {errors.name && (<span className={styles.errorFont}>{errors.name.message}</span>)}
+     <div className={styles.inputBoxContainer}>
+       <label className={styles.labelText}>생년월일</label>
+       <input type="date" className={styles.dateInputBox}
+              aria-invalid={isSubmitted ? (errors.birth ? 'true' : 'false') : undefined}
+              {...register("birth", {
+                required: "생년월일은 필수 입력입니다."
+              })} />
+     </div>
+     {errors.birth && (<span className={styles.errorFont1}>{errors.birth.message}</span>)}
+     <input type="text" className={styles.inputBoxExample} placeholder="인증번호 입력"/>
+     <button className={styles.subButton}>다음</button>
+   </form>
  );
 };
