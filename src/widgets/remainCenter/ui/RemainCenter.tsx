b98617380489676421ae2a80ff0e26a0d308a4cd
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-02-12 10:17:43 +0900

    Feat : 메인 중간 페이지 제작중(Donny : 2월 12일 10시18분)

--- src/widgets/remainCenter/ui/RemainCenter.tsx
+++ src/widgets/remainCenter/ui/RemainCenter.tsx
@@ -7,6 +7,12 @@ import developmentBg from '../../../shared/assets/icons/developmentBg.svg';
 
 import { useParams } from 'react-router';
 
+import styles from './RemainCenter.module.css'
+import userImage from '../../../shared/assets/icons/userImage.svg';
+import viewIcon from '../../../shared/assets/icons/viewIcon.svg';
+import commentIcon from '../../../shared/assets/icons/commentIcon.svg';
+import likeIcon from '../../../shared/assets/icons/likeIcon.svg';
+
 export const RemainCenter = () => {
   const { category } = useParams();
 
@@ -19,8 +25,44 @@ export const RemainCenter = () => {
   else if (category === 'free') imgSrc = freeBg;
 
   return (
-    <section>
-      <img src={imgSrc} alt='이미지 사진' />
+    <section className={styles.remainContainer}>
+      <img src={imgSrc} alt={imgSrc} className={styles.image} />
+      <aside className={styles.board}>
+        {Array.from({ length: 4 }, (_, i) => (
+          <div className={styles.boardList}>
+            <div className={styles.boardContainer}>
+              <p className={styles.badge}>자산증식</p>
+              <div className={styles.userContainer}>
+                <img src={userImage} alt={userImage} width={20} height={20} />
+                <span className={styles.userNameFont}>userName</span>
+              </div>
+              <p className={styles.boardContent}>제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목
+                제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목</p>
+              {/*<div className={styles.iconsContainer}>*/}
+              {/*  <div className={styles.icons}>*/}
+              {/*    <img src={viewIcon} alt={viewIcon} />*/}
+              {/*    <p className={styles.iconsFont}>106</p>*/}
+              {/*  </div>*/}
+              {/*  <div className={styles.icons}>*/}
+              {/*    <img src={commentIcon} alt={commentIcon} />*/}
+              {/*    <p className={styles.iconsFont}>106</p>*/}
+              {/*  </div>*/}
+              {/*  <div className={styles.icons}>*/}
+              {/*    <img src={likeIcon} alt={likeIcon} />*/}
+              {/*    <p className={styles.iconsFont}>106</p>*/}
+              {/*  </div>*/}
+              {/*  <div className={styles.numbers}>*/}
+              {/*    <span className={styles.bar}></span>*/}
+              {/*    <p className={styles.times}>1시간 전</p>*/}
+              {/*  </div>*/}
+              {/*</div>*/}
+            </div>
+            <span className={`${styles.boardBottom} ${i === 3 && styles.borderNone}`}></span>
+          </div>
+        ))}
+      </aside>
     </section>
   );
 };
+
+
