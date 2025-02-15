e1e01408448aeec75baf82dc0b0b10dc3346dd78
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-02-15 11:20:49 +0900

    Feat : 메인 Top5게시판 마크업 완료(Donny: 2월 15일 11시21분)

---
+++ src/widgets/remainCenter/ui/BottomMain.tsx
@@ -0,0 +1,45 @@
+import styles from './Bottom.module.css';
+
+import welfareIcon from '../../../shared/assets/icons/welfareMiniIcon.svg';
+// import homeIcon from '../../../shared/assets/icons/homeMiniIcon.svg'
+// import developmentIcon from '../../../shared/assets/icons/developmentMiniIcon.svg'
+// import qnaIcon from '../../../shared/assets/icons/qnaMiniIcon.svg'
+
+import viewIcon from '../../../shared/assets/icons/viewIcon.svg'
+import commentIcon from '../../../shared/assets/icons/commentIcon.svg';
+import likeIcon from '../../../shared/assets/icons/likeIcon.svg';
+
+export const BottomMain = () => {
+
+ return (
+   <section className={styles.boardContainer}>
+     {Array.from({ length: 5 }, (_, i) => {
+       return (
+         <article className={styles.contentBox} key={i}>
+           <h1 className={`${styles.boardRankNumber} ${i <= 2 ? styles.boardTop3 : styles.notboardTop3}`}>{i + 1}</h1>
+           <figure className={styles.iconContainer}>
+             <img src={welfareIcon} alt={welfareIcon} width={15} height={15} />
+             <figcaption className={styles.iconFont}>복지</figcaption>
+           </figure>
+           <section className={styles.iconsContainer}>
+             <figure className={styles.icons}>
+               <img src={viewIcon} alt={viewIcon} />
+               <figcaption className={styles.iconFont}>106</figcaption>
+             </figure>
+             <figure className={styles.icons}>
+               <img src={commentIcon} alt={commentIcon} />
+               <figcaption className={styles.iconFont}>106</figcaption>
+             </figure>
+             <figure className={styles.icons}>
+               <img src={likeIcon} alt={likeIcon} />
+               <figcaption className={styles.iconFont}>106</figcaption>
+             </figure>
+           </section>
+           <p className={styles.boardContent}>주짓수 vs 수영</p>
+           <span className={`${styles.borderBottom} ${i === 4 && styles.borderNone}`}></span>
+         </article>
+       )
+     })}
+   </section>
+ );
+};
--- src/widgets/remainCenter/ui/RemainCenter.tsx
+++ src/widgets/remainCenter/ui/RemainCenter.tsx
@@ -1,66 +1,23 @@
-import qnaBg from '../../../shared/assets/icons/qnaBg.svg';
-import freeBg from '../../../shared/assets/icons/freeBg.svg';
-import houseBg from '../../../shared/assets/icons/houseBg.svg';
-import welfareBg from '../../../shared/assets/icons/welfareBg.svg';
-import developmentBg from '../../../shared/assets/icons/developmentBg.svg';
-
-import { useParams } from 'react-router';
-
+import { TopMain } from './TopMain.tsx';
 import styles from './RemainCenter.module.css'
-import userImage from '../../../shared/assets/icons/userImage.svg';
-import viewIcon from '../../../shared/assets/icons/viewIcon.svg';
-import commentIcon from '../../../shared/assets/icons/commentIcon.svg';
-import likeIcon from '../../../shared/assets/icons/likeIcon.svg';
+import { BottomMain } from './BottomMain.tsx';
 
 export const RemainCenter = () => {
-  const { category } = useParams();
-
-  let imgSrc = houseBg;
-
-  if (category === 'home') imgSrc = houseBg;
-  else if (category === 'welfare') imgSrc = welfareBg;
-  else if (category === 'development') imgSrc = developmentBg;
-  else if (category === 'qna') imgSrc = qnaBg;
-  else if (category === 'free') imgSrc = freeBg;
 
   return (
-    <section className={styles.remainContainer}>
-      <img src={imgSrc} alt={imgSrc} className={styles.image} />
-      <aside className={styles.board}>
-        {Array.from({ length: 4 }, (_, i) => (
-          <div className={styles.boardList}>
-            <div className={styles.boardContainer}>
-              <p className={styles.badge}>자산증식</p>
-              <div className={styles.userContainer}>
-                <img src={userImage} alt={userImage} width={20} height={20} />
-                <span className={styles.userNameFont}>userName</span>
-              </div>
-              <p className={styles.boardContent}>제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목
-                제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목</p>
-              <div className={styles.iconsContainer}>
-                <div className={styles.icons}>
-                  <img src={viewIcon} alt={viewIcon} />
-                  <p className={styles.iconsFont}>106</p>
-                </div>
-                <div className={styles.icons}>
-                  <img src={commentIcon} alt={commentIcon} />
-                  <p className={styles.iconsFont}>106</p>
-                </div>
-                <div className={styles.icons}>
-                  <img src={likeIcon} alt={likeIcon} />
-                  <p className={styles.iconsFont}>106</p>
-                </div>
-                <div className={styles.numbers}>
-                  <span className={styles.bar}></span>
-                  <p className={styles.times}>1시간 전</p>
-                </div>
-              </div>
-            </div>
-            <span className={`${styles.boardBottom} ${i === 3 && styles.borderNone}`}></span>
-          </div>
-        ))}
-      </aside>
-    </section>
+    <>
+      <TopMain />
+      <article className={styles.container}>
+        <div className={styles.titleBox}>
+          <p className={styles.textStyle}>HOT</p>
+          <h1 className={styles.title}>게시글</h1>
+          <h3 className={styles.subTitle}>지금 가장 뜨거운 이야기!<br />
+            사람들이 공감한 인기 글을 만나보세요.
+          </h3>
+        </div>
+      </article>
+      <BottomMain />
+    </>
   );
 };
 
