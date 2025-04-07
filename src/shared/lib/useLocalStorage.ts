808593e100a94199d5c9845e3d1e42bf22e9c7d5
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-04-07 10:57:20 +0900

    Feat : Navbar 반응형 추가(Donny: 4월 7일 10시 57분)

---
+++ src/shared/lib/useLocalStorage.ts
@@ -0,0 +1,7 @@
+export const addLocalStorage = (key: string) => {
+  return window.localStorage.getItem(key);
+};
+
+export const removeLocalStorage = (key: string) => {
+  return window.localStorage.removeItem(key);
+};
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -5,6 +5,10 @@ import userImg from '../../../shared/assets/icons/userImg.svg';
 import { useEffect, useState } from 'react';
 import { BottomNavBar } from './BottomNavBar.tsx';
 import Logo from '../../../shared/assets/icons/logo.svg';
+import { getSession } from '../../../shared/lib/useSession.ts';
+import { addLocalStorage } from '../../../shared/lib/useLocalStorage.ts';
+import { UserTypes } from '../../../shared/types/googleUserTypes.ts';
+import { FiMenu } from 'react-icons/fi';
 
 export const NavBar = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -13,6 +17,7 @@ export const NavBar = () => {
   const navigate = useNavigate();
   const { category } = useParams();
   const location = useLocation();
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
   const moveLoginPage = () => {
     navigate('/login');
@@ -20,8 +25,13 @@ export const NavBar = () => {
 
   const handleRemoveData = () => {
     localStorage.removeItem('user');
+    localStorage.removeItem('userData');
     setIsLogin(false);
   };
+
+  const googleUserData = addLocalStorage('userData');
+  const realGoogleUserData: UserTypes | null =
+    googleUserData && JSON.parse(googleUserData);
 
   useEffect(() => {
     if (location.pathname.startsWith('/board')) {
@@ -30,6 +40,18 @@ export const NavBar = () => {
       setShow(false);
     }
   }, [location.pathname]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setInnerWidth(window.innerWidth);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [innerWidth]);
 
   useEffect(() => {
     const data = localStorage.getItem('user');
@@ -44,50 +66,46 @@ export const NavBar = () => {
   };
 
   return (
-    <nav>
-      <div className={styles.container}>
-        <ul className={styles.leftLinkContainer}>
-          <figure onClick={() => navigate('/')}>
-            <img src={Logo} alt={Logo} className={styles.icon} />
-          </figure>
-          <li
-            onClick={moveBoardPage}
-            className={category ? styles.pickFont : styles.linkfont}
-          >
-            사부작 게시판
-          </li>
-          <li className={styles.linkfont}>캘린더</li>
-          <li className={styles.linkfont}>사부작 순위</li>
-        </ul>
-        <NavBarSearchForm />
-        {isLogin ? (
-          <div className={styles.rightLinkContainer}>
-            <Link to='/' className={styles.linkText}>
-              알림
-            </Link>
-            <p onClick={handleRemoveData} className={styles.linkText}>
-              로그아웃
-            </p>
-            <figure>
-              <img
-                src={userImg}
-                alt='사용자 이미지'
-                className={styles.userImg}
-              />
-            </figure>
+    <nav className={styles.container}>
+      <ul className={styles.leftLinkContainer}>
+        {innerWidth <= 1260 ? (
+          <div className={styles.menuContainer}>
+            <FiMenu className={styles.menuIcon} />
+            <p className={styles.menuText}>사부작 사부작</p>
           </div>
         ) : (
-          <div className={styles.rightLinkContainer}>
-            <button
-              className={styles.whiteButtonStyles}
-              onClick={moveLoginPage}
-            >
-              로그인
-            </button>
-            <button className={styles.buttonStyle} onClick={moveLoginPage}>
-              회원가입
-            </button>
+          <img src={Logo} alt='사부작 로고' className={styles.icon} />
+        )}
+        <li
+          onClick={moveBoardPage}
+          className={category ? styles.pickFont : styles.linkfont}
+        >
+          사부작 게시판
+        </li>
+        <li className={styles.linkfont}>캘린더</li>
+        <li className={styles.linkfont}>사부작 순위</li>
+      </ul>
+      <div className={styles.rightLinkContainer}>
+        <NavBarSearchForm />
+        <ul className={styles.restContainer}>
+          <li className={styles.linkfont}>알림</li>
+          <li className={styles.linkfont}>메세지</li>
+        </ul>
+        {realGoogleUserData ? (
+          <div className={styles.userLogin}>
+            <img
+              src={realGoogleUserData.userImg}
+              alt='유저 이미지'
+              className={styles.userImage}
+            />
+            <p className={styles.loginBtn} onClick={handleRemoveData}>
+              로그아웃
+            </p>
           </div>
+        ) : (
+          <p className={styles.loginBtn} onClick={moveLoginPage}>
+            로그인
+          </p>
         )}
       </div>
       {show && <BottomNavBar />}
